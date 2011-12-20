81ed6b51ef34809c6d82ffd9dc14f8e7fb603b05
Jake Hiller <info@jakehiller.com>
2011-12-20 12:28:08 -0500

    added example config.js

---
+++ config_example.js
@@ -0,0 +1,10 @@
+// Rename this file to config.js t
+
+var config = {}
+
+config.room_id  = "your room number";
+config.account  = "the first part of your campfire url";
+config.token  = "your API token"
+config.ssl    = false;
+
+module.exports = config;
