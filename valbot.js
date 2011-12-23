8cfd1db1ed4d16323f40ffe5b1aeb25d9417d75c
Dan Ordille <dordille@gmail.com>
2011-12-23 15:13:22 -0500

    Added the ability to listen to commands to valbot.js

--- valbot.js
+++ valbot.js
@@ -7,6 +7,7 @@ var Val = function(instance, room_id) {
   this.instance = instance;
 
   events.EventEmitter.call(this);
+  
 };
 
 Val.super_ = events.EventEmitter;
@@ -19,9 +20,20 @@ Val.prototype = Object.create(events.EventEmitter.prototype, {
 
 Val.prototype.init = function(room) {
   var self = this;
+  self.on('TextMessage', self.command);
   room.listen(function(message){
     self.emit(message.type, room, message);
   })
+};
+
+Val.prototype.command = function(room, message) {
+  var re = /^\/([^\s]*) (.*)/;
+  console.log(message.body);
+  if(matches = message.body.match(re)) {
+    var command = matches[1];
+    var arguments = matches[2];
+    self.emit(command, arguments);
+  }
 };
 
 module.exports = Val;
