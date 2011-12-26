749a8c02c1660a9175447e169107a7d8fc2fea5d
Jake Hiller <jakemhiller@gmail.com>
2011-12-26 16:26:58 -0500

    added to config.js example file

--- config_example.js
+++ config_example.js
@@ -1,17 +1,17 @@
-// Rename this file to config.js t
+// Rename this file to config.js
 
 var config = {}
 
 config.room_id  = "your room number";
 config.account  = "the first part of your campfire url";
-config.token  = "your Campfire API token"
+config.token  = "your Campfire API key"
 config.ssl    = false;
 config.safesearch = false;
 
 // Commands
 
 //-- Image search
-config.bing       = 'your bing token'
+config.bing       = 'your bing API key'
 
 //-- lol (hosted folder full of animated gifs)
 config.lolFolder  = "http://lol.jaykillah.net/tmp/"
